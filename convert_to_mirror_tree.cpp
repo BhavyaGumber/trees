641929e7ef61d20d3699ffab125202958492e095
BhavyaGumber <79220494+BhavyaGumber@users.noreply.github.com>
2022-09-10 20:36:15 +0530

    Create convert_to_mirror_tree.cpp

---
+++ convert_to_mirror_tree.cpp
@@ -0,0 +1,11 @@
+ void mirror(Node* node) {
+        if(node==NULL)return;
+        else{
+           struct Node *temp;
+           mirror(node->left);
+           mirror(node->right);
+           temp=node->left;
+           node->left=node->right;
+           node->right=temp;
+            
+        }
